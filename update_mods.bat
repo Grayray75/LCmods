ee6f3c973db5e39bd330d3a6afde6f8448d16b2e
Grayray75 <69988482+Grayray75@users.noreply.github.com>
2024-02-22 19:09:44 +0100

    Update mods

--- update_mods.bat
+++ update_mods.bat
@@ -7,12 +7,12 @@ echo Downloading additional moons...
 mkdir ".\_download"
 
 del /s /q ".\BepInEx\plugins\Zingar-Atlas_Abyss\*" >NUL  2>NUL
-curl.exe --url "https://gcdn.thunderstore.io/live/repository/packages/Zingar-Atlas_Abyss-1.1.6.zip" --output ".\_download\Zingar-Atlas_Abyss-1.1.6.zip" -C -
-tar -xf ".\_download\Zingar-Atlas_Abyss-1.1.6.zip" -C ".\BepInEx\plugins\Zingar-Atlas_Abyss"
+curl.exe --url "https://gcdn.thunderstore.io/live/repository/packages/Zingar-Atlas_Abyss-1.1.8.zip" --output ".\_download\Zingar-Atlas_Abyss-1.1.8.zip" -C -
+tar -xf ".\_download\Zingar-Atlas_Abyss-1.1.8.zip" -C ".\BepInEx\plugins\Zingar-Atlas_Abyss"
 
 del /s /q ".\BepInEx\plugins\Zingar-SecretLabs\*" >NUL  2>NUL
-curl.exe --url "https://gcdn.thunderstore.io/live/repository/packages/Zingar-SecretLabs-3.3.5.zip" --output ".\_download\Zingar-SecretLabs-3.3.5.zip" -C -
-tar -xf ".\_download\Zingar-SecretLabs-3.3.5.zip" -C ".\BepInEx\plugins\Zingar-SecretLabs"
+curl.exe --url "https://gcdn.thunderstore.io/live/repository/packages/Zingar-SecretLabs-3.3.7.zip" --output ".\_download\Zingar-SecretLabs-3.3.7.zip" -C -
+tar -xf ".\_download\Zingar-SecretLabs-3.3.7.zip" -C ".\BepInEx\plugins\Zingar-SecretLabs"
 
 echo Download complete
 pause
