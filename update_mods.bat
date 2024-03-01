ba13ffe2149f358fe261f40b25bd1d57a6f1d6e1
Grayray75 <69988482+Grayray75@users.noreply.github.com>
2024-03-01 22:57:33 +0100

    I immediately regret this commit

--- update_mods.bat
+++ update_mods.bat
@@ -2,17 +2,33 @@ git reset --hard
 git fetch
 git pull
 
+GOTO:DOWNLOAD
+
+:downloadModPackage
+    SETLOCAL ENABLEDELAYEDEXPANSION
+        SET packageName=%~1
+        SET packageVersion=%~2
+
+        ECHO "Downloading %packageName% v%packageVersion%"
+        del /s /q ".\BepInEx\plugins\%packageName%\*" >NUL  2>NUL
+        curl.exe --url "https://gcdn.thunderstore.io/live/repository/packages/%packageName%-%packageVersion%.zip" --output ".\_download\%packageName%-%packageVersion%.zip" -C -
+        tar -xf ".\_download\%packageName%-%packageVersion%.zip" -C ".\BepInEx\plugins\%packageName%"
+    ENDLOCAL
+EXIT /B 0
+
+:DOWNLOAD
 @echo off
 echo Downloading additional moons...
 mkdir ".\_download"
 
-del /s /q ".\BepInEx\plugins\Zingar-Atlas_Abyss\*" >NUL  2>NUL
-curl.exe --url "https://gcdn.thunderstore.io/live/repository/packages/Zingar-Atlas_Abyss-1.1.8.zip" --output ".\_download\Zingar-Atlas_Abyss-1.1.8.zip" -C -
-tar -xf ".\_download\Zingar-Atlas_Abyss-1.1.8.zip" -C ".\BepInEx\plugins\Zingar-Atlas_Abyss"
-
-del /s /q ".\BepInEx\plugins\Zingar-SecretLabs\*" >NUL  2>NUL
-curl.exe --url "https://gcdn.thunderstore.io/live/repository/packages/Zingar-SecretLabs-3.3.7.zip" --output ".\_download\Zingar-SecretLabs-3.3.7.zip" -C -
-tar -xf ".\_download\Zingar-SecretLabs-3.3.7.zip" -C ".\BepInEx\plugins\Zingar-SecretLabs"
+call:downloadModPackage "KayNetsua-E_Gypt_Moon" "2.0.15"
+call:downloadModPackage "sfDesat-Orion" "2.0.1"
+call:downloadModPackage "Tolian-Celestria" "0.4.7"
+call:downloadModPackage "Tolian-EchoReach" "0.2.2"
+call:downloadModPackage "Tolian-Maritopia" "0.3.7"
+call:downloadModPackage "Tolian-PsychSanctum" "0.1.3"
+call:downloadModPackage "Zingar-Atlas_Abyss" "1.1.8"
+call:downloadModPackage "Zingar-SecretLabs" "3.3.7"
 
 echo Download complete
 pause
