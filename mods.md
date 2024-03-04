f508df91d8c193066dcb87847895ba8ed9a5dca0
Grayray75 <69988482+Grayray75@users.noreply.github.com>
2024-03-04 18:59:10 +0100

    Add most essential mods

---
+++ mods.md
@@ -0,0 +1,20 @@
+# Mods
+
+| Name                     | Version | Modified |
+| ------------------------ | ------- | -------- |
+| BepInEx                  | v5.4.21 |          |
+| BetterEmotes             | v1.4.5  |          |
+| GeneralImprovements      | v1.1.11 |          |
+| LateCompany              | v1.0.11 |          |
+| LCMaxSoundsFix           | v1.1.0  |          |
+| LethalCompany_InputUtils | v0.6.3  |          |
+| Mimics                   | v2.4.1  |          |
+| MoreCompany              | v1.7.6  |          |
+| OpenBodyCams             | v1.3.0  |          |
+| Runtime_Netcode_Patcher  | v0.2.5  |          |
+| YippeeMod                | v1.2.4  |          |
+
+## Modifications:
+
+| Name | Description |
+| ---- | ----------- |
---
+++ update_mods.bat
@@ -0,0 +1,61 @@
+:: LCMods update script
+
+echo Resetting and deleting old files...
+git reset --hard
+cd ".\BepInEx"
+git clean -fdx
+cd ".."
+
+echo Fetching mod updates...
+git fetch
+git pull
+
+GOTO:DOWNLOAD
+
+:downloadModPackage
+    SETLOCAL ENABLEDELAYEDEXPANSION
+        SET packageName=%~1
+        SET packageVersion=%~2
+
+        ECHO Downloading %packageName% v%packageVersion% ...
+        del /s /q ".\BepInEx\plugins\%packageName%\*" >NUL  2>NUL
+        curl.exe --url "https://gcdn.thunderstore.io/live/repository/packages/%packageName%-%packageVersion%.zip" --output ".\_download\%packageName%-%packageVersion%.zip" -C -
+        tar -xf ".\_download\%packageName%-%packageVersion%.zip" -C ".\BepInEx\plugins\%packageName%"
+    ENDLOCAL
+EXIT /B 0
+
+:deleteModPackage
+    SETLOCAL ENABLEDELAYEDEXPANSION
+        SET packageName=%~1
+        SET packageVersion=%~2
+
+        ECHO Deleting %packageName% v%packageVersion% ...
+        del /s /q ".\BepInEx\plugins\%packageName%\*" >NUL  2>NUL
+        del /s /q ".\_download\%packageName%-%packageVersion%.zip" >NUL  2>NUL
+    ENDLOCAL
+EXIT /B 0
+
+:DOWNLOAD
+@echo off
+
+echo Deleting old stuff...
+del /s /q ".\Dissonance_Diagnostics\*" >NUL  2>NUL
+rmdir /s /q ".\Dissonance_Diagnostics" >NUL  2>NUL
+call:deleteModPackage "Tolian-Celestria" "0.4.8"
+call:deleteModPackage "Tolian-EchoReach" "0.2.3"
+call:deleteModPackage "Tolian-Maritopia" "0.3.8"
+call:deleteModPackage "Tolian-PsychSanctum" "0.1.5"
+call:deleteModPackage "Zingar-Atlas_Abyss" "1.1.8"
+
+echo Downloading additional moons...
+mkdir ".\_download" >NUL  2>NUL
+
+call:downloadModPackage "AudioKnight-StarlancerMoons" "1.1.0"
+call:downloadModPackage "KayNetsua-E_Gypt_Moon" "2.0.15"
+call:downloadModPackage "Major_And_Skiz-MoreInteriors" "2.5.0"
+call:downloadModPackage "sfDesat-Orion" "2.0.1"
+call:downloadModPackage "Zingar-SecretLabs" "3.3.7"
+
+echo Download complete
+pause
+@echo on
