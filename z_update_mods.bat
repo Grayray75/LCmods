bc6b07731a65684679dc7e68c1b93ab9e74bdf43
Grayray75 <69988482+Grayray75@users.noreply.github.com>
2024-11-01 10:08:45 +0100

    Changes

--- z_update_mods.bat
+++ z_update_mods.bat
@@ -49,9 +49,12 @@ echo:
 echo Downloading additional moons...
 mkdir ".\_download" >NUL  2>NUL
 
-::call:deleteModPackage "Evaisa-LethalThings" "0.10.2"
+call:deleteModPackage "Skeleton_Studios-Welcome_To_Ooblterra" "1.2.0"
+
 call:downloadModPackage "mrgrm7-LethalCasino" "1.1.2"
-call:downloadModPackage "Skeleton_Studios-Welcome_To_Ooblterra" "1.2.0"
+call:downloadModPackage "RealeStudios-CastleGrounds" "5.1.0"
+call:downloadModPackage "sfDesat-Aquatis" "2.2.3"
+call:downloadModPackage "Zingar-SecretLabs" "3.4.1"
 
 git reset --hard
 
