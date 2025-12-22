58b9d996a4bca8603fcf8246377bf70087c06f16
Grayray75 <69988482+Grayray75@users.noreply.github.com>
2025-12-22 11:15:49 +0100

    more stuff

---
+++ z_update_mods.bat
@@ -0,0 +1,17 @@
+:: LCMods update script
+
+echo Resetting and deleting old files...
+git reset --hard
+cd ".\BepInEx"
+git clean -fdx
+cd ".."
+
+echo Fetching mod updates...
+git fetch
+git pull
+
+echo Removing old git data...
+:: https://stackoverflow.com/a/46004595
+git fetch --depth 1
+git branch -d V3
+git stash drop
